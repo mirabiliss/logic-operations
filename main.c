3ae93abd1b981230cec3da5abd0d9a672bc08ba8
Yana <58123871+mirabiliss@users.noreply.github.com>
2020-09-20 22:13:47 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 #define OR(x, y) ((x)? 1: ((y)? 1 : 0))
 #define AND(x, y) ((x)? ((y)? 1 : 0) : 0)
-#define ALT_OR(x, y) ((x)? ((!y)? 1: 0) :((y)? 1: 0))
+#define XOR(x, y) ((x)? ((!y)? 1: 0) :((y)? 1: 0))
 #define NOT(x) ((x)? 0: 1)
 #define FIRST_LINE(x, y, z)  (AND(AND(AND(x, y), OR(x, z)), ALT_OR(NOT(x), y)))
 #define SECOND_LINE(x, y, z)  (AND(AND(OR(x, y), OR(z, y)), OR(z, x)))
@@ -21,7 +21,9 @@ int main(void) {
 	scanf_s("%d", &y);
 	printf("enter z: ");
 	scanf_s("%d", &z);
+	
 	w = RESULT(x, y, z);
 	printf("result is: %d", w);
+	
 	return 0;
 }
